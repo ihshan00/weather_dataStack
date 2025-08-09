d3b3df2d97d6940db8fe5227d8f3e5063979e300
ihshan00 <87473778+ihshan00@users.noreply.github.com>
2025-08-09 18:03:53 +0000

    updated the compose

--- weather_pipeline/docker/entrypoint.sh
+++ weather_pipeline/docker/entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 if [ -e "/opt/airflow/requirements.txt" ]; then
-  $(command python) pip install --upgrade pip
-  $(command -v pip) install --user -r ./requirements.txt
+  $(command -v python) -m pip install --upgrade pip
+  $(command -v pip) install -r /opt/airflow/requirements.txt
 fi
 
 if [ ! -f "/opt/airflow/airflow.db" ]; then
-  airflow db init && \
-  airflow users create \
+  $(command -v airflow) db init
+  $(command -v airflow) users create \
     --username admin \
     --firstname admin \
     --lastname admin \
@@ -17,6 +17,6 @@ if [ ! -f "/opt/airflow/airflow.db" ]; then
     --password admin
 fi
 
-$(command -v airflow) db upgrade
+$(command -v airflow) db migrate
 
-exec airflow webserver
+exec $(command -v airflow) webserver
