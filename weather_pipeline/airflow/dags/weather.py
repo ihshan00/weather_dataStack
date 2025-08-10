49c4b92bcee12e14ec676e699ce475a97d0be157
ihshan00 <87473778+ihshan00@users.noreply.github.com>
2025-08-10 10:02:18 +0530

    Update weather.py

--- weather_pipeline/airflow/dags/weather.py
+++ weather_pipeline/airflow/dags/weather.py
@@ -12,6 +12,21 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+"""
+In Airflow Admin → Connections:
+
+Create a new connection with:
+
+    Conn Id: spark_default
+
+    Conn Type: Spark
+
+    Master URL: spark://spark-master:7077
+
+    Deploy mode: client
+"""
+
+
 with DAG(
     dag_id='weather_pipeline',
     default_args=default_args,
@@ -24,3 +39,25 @@ with DAG(
         task_id='fetch_and_publish',
         python_callable=lambda: __import__('../ingestion.ingest').publish()
     )
+    consume_kafka_task = SparkSubmitOperator(
+            task_id="consume_kafka_task",
+            application="/opt/airflow/dags/consume_kafka.py",  
+            conn_id="spark_default",  
+            verbose=True,
+            master="spark://spark-master:7077",  
+            packages=(
+                "org.apache.spark:spark-sql-kafka-0-10_2.13:3.3.2,"
+                "io.delta:delta-core_2.13:2.3.0,"
+                "org.apache.hadoop:hadoop-aws:3.3.4"
+            ),
+            name="ConsumeKafkaToDelta",
+            conf={
+                "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
+                "spark.hadoop.fs.s3a.access.key": "minioadmin",
+                "spark.hadoop.fs.s3a.secret.key": "minioadmin",
+                "spark.hadoop.fs.s3a.path.style.access": "true",
+                "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
+                "spark.delta.logStore.class": "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore",
+            },
+        )
+    ingest >> consume_kafka_task
