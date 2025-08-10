75748e4cb9dc45d71890fa4ca02ff51b852d2107
ihshan00 <87473778+ihshan00@users.noreply.github.com>
2025-08-10 20:28:39 +0530

    Update entrypoint.sh

--- weather_pipeline/docker/entrypoint.sh
+++ weather_pipeline/docker/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-2.6.0/constraints-3.9.txt"
+export PATH="$PATH:/home/airflow/.local/bin"
 
-if [ -s "/opt/airflow/requirements.txt" ]; then
+if [ -f "/opt/airflow/requirements.txt" ]; then
   python -m pip install --upgrade pip
-  pip install --no-cache-dir -r /opt/airflow/requirements.txt --constraint "${CONSTRAINT_URL}"
+  pip install -r /opt/airflow/requirements.txt
 fi
 
-# use python -m airflow to avoid PATH issues
 if [ ! -f "/opt/airflow/airflow.db" ]; then
   python -m airflow db init
   python -m airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin@example.com --password admin || true
